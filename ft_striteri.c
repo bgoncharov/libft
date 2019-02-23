06926928005b5bd97884168a78bf4789312049be
Boris Goncharov <bogoncha@e1z2r8p38.42.us.org>
2019-02-22 16:23:03 -0800

    prettify

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: bogoncha <bogoncha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 09:36:10 by bogoncha          #+#    #+#             */
-/*   Updated: 2019/02/15 22:04:01 by bogoncha         ###   ########.fr       */
+/*   Updated: 2019/02/22 16:21:51 by bogoncha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	size_t i;
+	unsigned int i;
 
-	if (!s || !f)
-		return ;
 	i = 0;
-	while (*s)
+	if (s != NULL && f != NULL)
 	{
-		f(i, s);
-		i++;
-		s++;
+		while (*s)
+		{
+			(*f)(i, s);
+			i++;
+			s++;
+		}
 	}
 }

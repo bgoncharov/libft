8965d0eed8eec8cf1e84d26c1160157e19d4a702
Boris Goncharov <bogoncha@e1z2r8p38.42.us.org>
2019-02-22 17:23:49 -0800

    prettify

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,69 +6,52 @@
 /*   By: bogoncha <bogoncha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/17 11:51:17 by bogoncha          #+#    #+#             */
-/*   Updated: 2019/02/22 17:04:11 by bogoncha         ###   ########.fr       */
+/*   Updated: 2019/02/22 17:22:16 by bogoncha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static int			ft_cntwrd(char const *s, char c)
+static int	fill_arr(char const *s, char *arr, unsigned int i, char c)
 {
-	unsigned int	i;
-	int				cntr;
+	int j;
+
+	j = 0;
+	while (s[i] != c && s[i])
+	{
+		arr[j] = s[i];
+		i++;
+		++j;
+	}
+	arr[j] = '\0';
+	return (i);
+}
+
+char		**ft_strsplit(char const *s, char c)
+{
+	int		i;
+	int		j;
+	char	**arr;
 
 	i = 0;
-	cntr = 0;
-	while (s[i])
+	j = 0;
+	if (!s)
+		return (NULL);
+	arr = (char **)malloc(sizeof(char *) * ft_strlen(s));
+	if (!arr)
+		return (NULL);
+	while (s[i] != '\0')
 	{
-		while (s[i] == c)
+		while (s[i] == c && s[i] != '\0')
 			i++;
 		if (s[i] != '\0')
-			cntr++;
-		while (s[i] && (s[i] != c))
-			i++;
-	}
-	return (cntr);
-}
-
-static char			*ft_strndup(const char *s, size_t n)
-{
-	char			*str;
-
-	str = (char *)malloc(sizeof(char) * n + 1);
-	if (str == NULL)
-		return (NULL);
-	str = ft_strncpy(str, s, n);
-	str[n] = '\0';
-	return (str);
-}
-
-char				**ft_strsplit(char const *s, char c)
-{
-	int				i;
-	int				j;
-	int				k;
-	char			**tab;
-
-	i = 0;
-	k = 0;
-	tab = (char **)malloc(sizeof(char *) * (ft_cntwrd(s, c)) + 1);
-	if (tab == NULL)
-		return (NULL);
-	while (s[i])
-	{
-		while (s[i] == c)
-			i++;
-		j = i;
-		while (s[i] && s[i] != c)
-			i++;
-		if (i > j)
 		{
-			tab[k] = ft_strndup(s + j, i - j);
-			k++;
+			arr[j] = ft_strnew(ft_size_of_word(&s[i], c));
+			i = fill_arr(s, arr[j], i, c);
+			j++;
 		}
 	}
-	tab[k] = NULL;
-	return (tab);
+	arr[j] = NULL;
+	return (arr);
 }

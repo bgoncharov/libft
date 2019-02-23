6454450fd5e645debd004c5e23975c96b62d3760
Boris Goncharov <bogoncha@e1z2r8p38.42.us.org>
2019-02-22 16:31:59 -0800

    prettify

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: bogoncha <bogoncha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 22:45:28 by bogoncha          #+#    #+#             */
-/*   Updated: 2019/02/15 23:02:36 by bogoncha         ###   ########.fr       */
+/*   Updated: 2019/02/22 16:31:26 by bogoncha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 int		ft_strequ(char const *s1, char const *s2)
 {
-	if (!s1 || !s2)
-		return (-1);
-	if (ft_strcmp(s1, s2) == 0)
-		return (1);
+	int i;
+
+	i = 0;
+	if (!s1 || !s2 || ft_strlen(s1) != ft_strlen(s2))
+		return (0);
+	while (s1[i] == s2[i])
+	{
+		if (!s1[i] && !s2[i])
+			return (1);
+		i++;
+	}
 	return (0);
 }
